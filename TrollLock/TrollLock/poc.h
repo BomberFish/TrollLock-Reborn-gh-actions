65c1d9c8379ce3592691f589480eeaa5f19316cd
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-24 09:25:49 -0500

    Implement Ian Beer's PoC

--- TrollLock/TrollLock/poc.h
+++ TrollLock/TrollLock/poc.h
@@ -1,13 +1,52 @@
-//
-//  poc.h
-//  POCTester
-//
-//  Created by Анохин Юрий on 20.12.2022.
-//
+// missing prototypes:
 
-#ifndef poc_h
-#define poc_h
+kern_return_t mach_vm_allocate
+ (
+  vm_map_t target,
+  mach_vm_address_t *address,
+  mach_vm_size_t size,
+  int flags
+  );
 
-#include <stdio.h>
-void trollLockCopy(NSString *sourceFilePath, NSString *targetFilePath);
-#endif /* poc_h */
+kern_return_t mach_vm_deallocate
+ (
+  vm_map_t target,
+  mach_vm_address_t address,
+  mach_vm_size_t size
+  );
+
+kern_return_t mach_vm_read_overwrite
+ (
+  vm_map_read_t target_task,
+  mach_vm_address_t address,
+  mach_vm_size_t size,
+  mach_vm_address_t data,
+  mach_vm_size_t *outsize
+  );
+
+kern_return_t mach_vm_map
+ (
+  vm_map_t target_task,
+  mach_vm_address_t *address,
+  mach_vm_size_t size,
+  mach_vm_offset_t mask,
+  int flags,
+  mem_entry_name_port_t object,
+  memory_object_offset_t offset,
+  boolean_t copy,
+  vm_prot_t cur_protection,
+  vm_prot_t max_protection,
+  vm_inherit_t inheritance
+  );
+
+kern_return_t mach_vm_protect
+(
+        vm_map_t target_task,
+        mach_vm_address_t address,
+        mach_vm_size_t size,
+        boolean_t set_maximum,
+        vm_prot_t new_protection
+);
+
+void trollLockCopy(NSData *data, NSString *path);
+
